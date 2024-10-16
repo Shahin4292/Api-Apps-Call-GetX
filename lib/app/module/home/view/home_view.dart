da78c5df1509502374c134b52fb475a5e2d30655
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-16 14:22:57 +0600

    home view done

--- lib/app/module/home/view/home_view.dart
+++ lib/app/module/home/view/home_view.dart
@@ -9,26 +9,48 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
-        ],
-        centerTitle: true,
-        title: const Text("Home View"),
-      ),
-      body: SafeArea(
-        child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return ContactCard(
-                name: '',
-                email: '',
-                address: '',
-                city: '',
-                country: '',
-              );
-            }),
-      ),
-    );
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  controller.fetchContact();
+                },
+                icon: const Icon(Icons.refresh)),
+          ],
+          centerTitle: true,
+          title: const Text("Home View"),
+        ),
+        body: FutureBuilder(
+          future: controller.fetchContact(),
+          builder: (ctx, snapshot) {
+            if (snapshot.hasData) {
+              return Obx(() => ListView.builder(
+                  itemCount: controller.contact.value?.profiles?.length,
+                  itemBuilder: (context, index) {
+                    return Obx(() => ContactCard(
+                          name:
+                              controller.contact.value?.profiles?[index].name ??
+                                  '',
+                          email: controller
+                                  .contact.value?.profiles?[index].email ??
+                              '',
+                          address: controller
+                                  .contact.value?.profiles?[index].address ??
+                              '',
+                          city:
+                              controller.contact.value?.profiles?[index].city ??
+                                  '',
+                          country: controller
+                                  .contact.value?.profiles?[index].country ??
+                              '',
+                        ));
+                  }));
+            } else if (snapshot.hasError) {
+              return const Center(child: Text("Have an error"));
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
