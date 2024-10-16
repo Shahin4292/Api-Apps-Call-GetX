0845057bc899ddd0c8636d2fb5742c6296edad79
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-16 10:43:57 +0600

    home controller done

--- lib/app/module/home/view/home_view.dart
+++ lib/app/module/home/view/home_view.dart
@@ -1,17 +1,33 @@
+import 'package:api_call_getx/app/module/home/controller/home_controller.dart';
+import 'package:api_call_getx/app/widget/contact_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomeView extends GetView {
+class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
+        ],
+        centerTitle: true,
+        title: const Text("Home View"),
+      ),
       body: SafeArea(
-        child: ListView.builder(itemBuilder: (context, index) {
-          return Contr
-        }),
+        child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ContactCard(
+                name: '',
+                email: '',
+                address: '',
+                city: '',
+                country: '',
+              );
+            }),
       ),
     );
   }
